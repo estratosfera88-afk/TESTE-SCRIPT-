5774d937e46755f1152b7512a0adc0f08a1fbb1f
estratosfera88-afk <estratosfera88@gmail.com>
2026-08-24 21:26:03 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1812,7 +1812,7 @@ local function ExecutarIntroAkat()
     
     CriarNotificacao(
         "AKATSUKI SCRIPTS",
-        "MM2 Script carregado com sucesso! Bem-vindo, " .. player.DisplayName .. "."
+        "MM2 Script carregado com sucesso. Bem-vindo, " .. player.DisplayName .. "."
     )
     
     task.wait(0.8)
